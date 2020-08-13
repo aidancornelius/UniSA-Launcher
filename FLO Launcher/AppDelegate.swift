b12cc217e5e36859eb1836d72883aeac9fbee49c
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2020-08-13 10:39:18 +0930

    Initial Commit

---
+++ FLO Launcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FLO Launcher
+//
+//  Created by Aidan Cornelius-Bell on 13/8/20.
+//  Copyright © 2020 Aidan Cornelius-Bell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FLO Launcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FLO Launcher
+//
+//  Created by Aidan Cornelius-Bell on 13/8/20.
+//  Copyright © 2020 Aidan Cornelius-Bell. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
