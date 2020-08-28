f126dec7ab1a7a24fa534907475f2488c21f81fe
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2020-08-28 13:34:07 +0930

    Basic working version.

--- FLO Launcher/ViewController.swift
+++ FLO Launcher/ViewController.swift
@@ -8,20 +8,86 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, NSTextFieldDelegate, NSSearchFieldDelegate {
+    
+    @IBOutlet weak var topicSearchtext: NSSearchField!
+    @IBOutlet weak var topicIdentifier: NSSearchField!
+    @IBOutlet weak var directorySearch: NSSearchField!
+    
+    
+    @IBAction func jumpToTopics(_ sender: Any) {
+        NSLog(topicIdentifier.stringValue)
+        
+        
+        if let url = URL(string: "https://flo.flinders.edu.au/course/view.php?id="+topicIdentifier.stringValue), NSWorkspace.shared.open(url) {
+            print("default browser was successfully opened")
+        }
+    }
+    
+    @IBAction func searchTopics(_ sender: Any) {
+        NSLog(topicSearchtext.stringValue)
+        
+        if let url = URL(string: "https://flo.flinders.edu.au/course/search.php?search="+topicSearchtext.stringValue.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)! ?? ""), NSWorkspace.shared.open(url) {
+            print("default browser was successfully opened")
+        }
+    }
+    
+    @IBAction func searchPeople(_ sender: Any) {
+        NSLog(directorySearch.stringValue)
+        
+        var theUrl = "https://www.flinders.edu.au/people?q=" + directorySearch.stringValue.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)! ?? ""
+        theUrl = theUrl + "#results-search-response"
+        
+        if let url = URL(string: theUrl), NSWorkspace.shared.open(url) {
+            print("default browser was successfully opened")
+        }
+    }
+    
+    func control(_ control: NSControl, textView: NSTextView, doCommandBy commandSelector: Selector) -> Bool {
+        if control.tag == 0 {
+            if (commandSelector == #selector(NSResponder.insertNewline(_:))) {
+                // Do something against ENTER key
+                jumpToTopics(self)
+                return true
+            } else {
+                return false
+            }
+        } else if control.tag == 1 {
+            if (commandSelector == #selector(NSResponder.insertNewline(_:))) {
+                searchTopics(self)
+                return true
+            } else {
+                return false
+            }
+        } else {
+            if (commandSelector == #selector(NSResponder.insertNewline(_:))) {
+                searchPeople(self)
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
         }
     }
 
 
 }
 
+extension ViewController {
+    static func freshController() -> ViewController {
+        let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+        let identifier = NSStoryboard.SceneIdentifier("ViewController")
+        guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? ViewController else {
+          fatalError("No VC available.")
+        }
+        return viewcontroller
+    }
+}
